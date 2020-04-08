3e15e944cd407542d571eebeb1609007c798d526
Ranier <sskranier@gmail.com>
2020-04-08 12:49:19 -0300

    08/04

--- src/main/java/com/api/apirest/models/Model.java
+++ src/main/java/com/api/apirest/models/Model.java
@@ -1,8 +1,28 @@
 package com.api.apirest.models;
 
-public class Model {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="TB_PRODUTO")
+public class Model implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
 	private String linha;
+	@NotNull
 	private String modelo;
 	
 	
